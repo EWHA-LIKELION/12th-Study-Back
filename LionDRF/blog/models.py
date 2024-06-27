f4e2bff3f044181e39365473337b341351316870
minsihihi <qkralstj@gmail.com>
2024-06-27 18:30:57 +0900

    박민서:240627

--- LionDRF/blog/models.py
+++ LionDRF/blog/models.py
@@ -1,20 +1,50 @@
 from django.db import models
-
+import os
+from django.utils import timezone
+from api.models import *
 # Create your models here.
-
-LANGUAGE_CHOICES = (
-    (1, "KOR"),
-    (2, "ENG"),
-    (3, "JPN"),
-    (4, "CHN"),
+SUBJECTS = (
+    ('교육, 학문', '교육, 학문'),
+    ('컴퓨터통신', '컴퓨터통신'),
+    ('게임', '게임'),
+    ('엔터테인먼트, 예술', '엔터테인먼트, 예술'),
+    ('생활', '생활'),
+    ('건강', '건강'),
+    ('사회, 정치', '사회, 정치'),
+    ('경제', '경제'),
+    ('여행', '여행'),
+    ('스포츠, 레저', '스포츠, 레저'),
+    ('쇼핑', '쇼핑'),
+    ('지역&플레이스', '지역&플레이스'),
+    ('고민Q&A', '고민Q&A')
 )
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateTimeField('date published')
-    body = models.TextField()
-    language = models.IntegerField(choices=LANGUAGE_CHOICES)
+    title = models.CharField('제목', max_length=50)
+    user = models.ForeignKey(User, related_name="writer",
+                             on_delete=models.CASCADE, null=True)
+    like_users = models.ManyToManyField(User, related_name="like_question")
+    upload_time = models.DateTimeField(default=timezone.now)
+    content = models.TextField('내용', null=True)
+    image = models.ImageField('사진', blank=True)
+    category = models.CharField(
+        '분야', max_length=20, choices=SUBJECTS, default='uncategorized')
 
     def __str__(self):
         return self.title
+
+    def get_filename(self):
+        return os.path.basename(self.file.name)
+
+
+class Comment(models.Model):
+    question = models.ForeignKey(
+        Question, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="answer",
+                             on_delete=models.CASCADE, null=True)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.comment_text
